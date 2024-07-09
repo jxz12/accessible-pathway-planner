129982db67af955f457bf78a17ebcb09556fbd42
Jonathan Zheng <jxz12@ic.ac.uk>
2024-07-09 22:56:49 +0100

    made thing clickable!

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import { useState } from "react";
-import Map, { FullscreenControl, NavigationControl, ScaleControl } from 'react-map-gl';
+import pinsvg from "./assets/react.svg";
+
+import Map, { FullscreenControl, NavigationControl, ScaleControl, Marker } from 'react-map-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';  // NOTE: this is needed for controls to render correctly
 
 
 export default function App() {
   const [viewState, setViewState] = useState({
-    latitude: 43.6635,
     longitude: -79.3961,
+    latitude: 43.6635,
     zoom: 14.5,
   });
+  const [pins, setPins] = useState([
+    { longitude: -79.3961, latitude: 43.6635},
+  ]);
+  const addPin = (mapEvent) => {
+    console.log(mapEvent);
+    setPins([...pins, { longitude: mapEvent.lngLat.lng, latitude: mapEvent.lngLat.lat }]);
+  };
+
   return (
-    <>
-      <Map
-        mapLib={import("maplibre-gl")}
-        {...viewState}
-        onMove={evt => setViewState(evt.viewState)}
-        style={{ width: "100%", height: "95vh" }}
-        mapStyle="https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
-      >
-        <FullscreenControl />
-        <NavigationControl />
-        <ScaleControl />
-      </Map>
-    </>
+    <Map
+      mapLib={import("maplibre-gl")}
+      {...viewState}
+      onMove={evt => setViewState(evt.viewState)}
+      style={{ width: "100%", height: "95vh" }}
+      mapStyle="https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
+      onClick={addPin}
+    >
+      <FullscreenControl />
+      <NavigationControl />
+      <ScaleControl />
+      {pins.map((pin) => (
+        <Marker longitude={pin.longitude} latitude={pin.latitude} anchor="bottom">
+          <img src={pinsvg} />
+        </Marker>
+      ))}
+    </Map>
   );
 }
