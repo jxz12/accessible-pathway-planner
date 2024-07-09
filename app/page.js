1c5208775b9cfc5091af2f96edf9ba736099ce02
Jonathan Zheng <jxz12@ic.ac.uk>
2024-07-09 20:43:43 +0100

    broken controls

--- app/page.js
+++ app/page.js
@@ -14,16 +14,23 @@ export default function Home() {
   useEffect(() => {
     const map = new maplibregl.Map({
       container: mapContainer.current,
-      style: "https://demotiles.maplibre.org/style.json",
+      // style: "https://demotiles.maplibre.org/style.json",
+      // style: "https://demotiles.maplibre.org/styles/osm-bright-gl-style/style.json",
+      // style: "https://github.com/maplibre/demotiles/blob/gh-pages/styles/osm-bright-gl-style/style.json",
+      style: "https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
       ...view,
     });
+    // map.addControl(new maplibregl.ScaleControl(), "bottom-right");
+    map.addControl(new maplibregl.FullscreenControl());
+    // map.addControl(new maplibregl.NavigationControl(), "bottom-right");
+    // map.addControl(new maplibregl.GeolocateControl(), "bottom-right");
     return () => {
       map.remove();
     };
   }, []);
   return (
     <>
-      <div ref={mapContainer} class="w-full h-full absolute"></div>
+      <div ref={mapContainer} className="w-full h-full absolute"></div>
     </>
   );
 }
