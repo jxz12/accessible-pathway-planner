07f26ae1f4be2f3070f03e551b017a8980fce37c
Jonathan Zheng <jxz12@ic.ac.uk>
2024-07-09 22:32:08 +0100

    swap to controlled map

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useRef, useState, useEffect } from "react";
+import { useState } from "react";
 import Map from 'react-map-gl';
-import 'maplibre-gl/dist/maplibre-gl.css'
+// import 'maplibre-gl/dist/maplibre-gl.css'
 
 
 export default function App() {
+  const [viewState, setViewState] = useState({
+    latitude: 43.6635,
+    longitude: -79.3961,
+    zoom: 14.5,
+  });
   return (
     <>
-      {/* <link href='https://unpkg.com/maplibre-gl@4.5.0/dist/maplibre-gl.css' rel='stylesheet'/> */}
       <Map
         mapLib={import("maplibre-gl")}
-        initialViewState={{ longitude: -100, latitude: 40, zoom: 3.5 }}
-        style={{ width: 500, height: 500 }}
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+        style={{ width: "100%", height: "95vh" }}
         mapStyle="https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
       />
     </>
