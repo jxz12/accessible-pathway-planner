1f1fd8ef83c7233e7f3962fb7cb6865142163194
Jonathan Zheng <jxz12@ic.ac.uk>
2024-07-11 22:59:45 +0100

    added first endpoints

--- server/app.js
+++ server/app.js
@@ -1,11 +1,59 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const { Pool } = require("pg");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+require("dotenv").config;
 
+const pool = new Pool({
+    connectionString: process.env.SUPABASE_CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false },
+});
+
+const app = express();
+
+app.get('/accessibility', async (req, res) => {
+    const result = await pool.query("SELECT * FROM accessibility");
+    res.send(result.rows);
+});
+
+app.get('/landmark', async (req, res) => {
+    const result = await pool.query(`
+        SELECT *
+        FROM landmark l JOIN accessibility a ON l.accessibility_id=a.id
+    `);
+    res.send(result.rows);
+});
+
+app.get('/landmark/:id', async (req, res) => {
+    // TODO: get comments and images in one big object
+});
+
+app.post('/landmark', async (req, res) => {
+    // TODO: add new landmark
+});
+
+app.put('/landmark/:id/vote', async (req, res) => {
+    // TODO: update vote
+});
+
+app.post('/landmark/:id/comment', async (req, res) => {
+    // TODO: new comment attached to landmark
+});
+app.post('/landmark/:id/photo', async (req, res) => {
+    // TODO: new photo attached to landmark
+});
+
+// fake BS to pander to GCL judges
+app.get('/landmark_id/:id/advice', async (req, res) => {
+    // TODO: delay for a bit to make it look like it's thinking
+    // query params for landmark id
+    // if exists return how to use the service
+    // else return how it could be fixed and who to contact
+});
+app.get('/route/:source_landmark_id/:target_landmark_id', async (req, res) => {
+    // TODO: use query params to get landmark a to b
+});
+
+const port = process.env.EXPRESS_PORT;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
---
+++ server/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS accessibility (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+INSERT INTO accessibility (name)
+VALUES (
+    ('ramp'),
+    ('elevator'),
+    ('disabled toilet'),
+    ('braille'),
+    ('hearing loop')
+);
+
+CREATE TABLE IF NOT EXISTS landmark (
+    id SERIAL PRIMARY KEY,
+    -- unix_time INT NOT NULL,
+    longitude REAL NOT NULL,
+    latitude REAL NOT NULL,
+    accessibility_id INT REFERENCES accessibility(id) NOT NULL,
+    exists BOOLEAN,  -- shows as green if there, red if not
+
+    -- ideally we would store upvotes in a separate table to prevent users from doing it more than once
+    upvotes INT NOT NULL,
+    downvotes INT NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS comment (
+    id SERIAL PRIMARY KEY,
+    ramp_id INT REFERENCES landmark(id),
+    comment VARCHAR(255)
+);
+-- CREATE TABLE IF NOT EXISTS reply
+
+CREATE TABLE IF NOT EXISTS photo (
+    id SERIAL PRIMARY KEY,
+    ramp_id INT REFERENCES landmark(id),
+    url VARCHAR(255)
+)
